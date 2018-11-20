c4f3b7d101750255a7120301e2a3bb11d2c8b8d4
VitaliZahar <vitalizahar@gmail.com>
2018-11-20 12:43:09 -0800

    Change project structure

---
+++ AnimationTask2/Constants/Consts.swift
@@ -0,0 +1,14 @@
+//
+//  Consts.swift
+//  AnimationTask2
+//
+//  Created by vitali on 11/20/18.
+//  Copyright © 2018 vitcopr. All rights reserved.
+//
+
+import Foundation
+
+struct Consts {
+    
+    
+}
